777e56716885b70deae38bf19ae2bdacd15d644c
VirtualInstict <jean-michel@pothelune.fr>
2014-01-11 16:49:59 +0100

    Initial commit

---
+++ src/main/java/org/tse/pri/ioarmband/armband/App.java
@@ -0,0 +1,13 @@
+package org.tse.pri.ioarmband.armband;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
