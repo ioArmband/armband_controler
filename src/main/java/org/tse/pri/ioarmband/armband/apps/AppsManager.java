1102d7b7f8f5745b3679fd1356287c8f1bb857f4
VirtualInstict <jean-michel@pothelune.fr>
2014-02-09 20:59:45 +0100

    Add swipe gesture emulation with keyboard

--- src/main/java/org/tse/pri/ioarmband/armband/apps/AppsManager.java
+++ src/main/java/org/tse/pri/ioarmband/armband/apps/AppsManager.java
@@ -1,5 +1,12 @@
 package org.tse.pri.ioarmband.armband.apps;
 
+import java.awt.AWTEvent;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -15,10 +22,11 @@ import org.tse.pri.ioarmband.armband.io.Client;
 import org.tse.pri.ioarmband.io.message.GestureMessage;
 import org.tse.pri.ioarmband.io.message.enums.GestureType;
 
+
 public class AppsManager implements AppListener, InputListener{
 
 	private static final Logger logger = Logger.getLogger(AppsManager.class);
-	
+
 	DisplayEngine displayEngine;
 
 	App currentApp;
@@ -46,6 +54,8 @@ public class AppsManager implements AppListener, InputListener{
 		apps = new LinkedList<App>(); 
 		clientsApp = new HashMap<Client, App>();
 		currentApp = null;
+
+		listenKeyboard();
 	}
 
 	public void addApp(App app){
@@ -92,7 +102,7 @@ public class AppsManager implements AppListener, InputListener{
 			setCurrentApp(currentApp);
 		}
 	}
-	
+
 	public void removeClient(Client client){
 		App app = clientsApp.get(client);
 		removeApp(app);
@@ -134,16 +144,53 @@ public class AppsManager implements AppListener, InputListener{
 					else
 						direction = "down";
 				}
-				
+
 				if(currentApp != null){
 					Client client = currentApp.getClient();
 					if(client != null)
 						client.sendCommand(new GestureMessage(GestureType.SWIPE, direction));
 				}
-				
+
 			}
 		}
 	}
 
+	public void listenKeyboard(){
+		KeyEventDispatcher listener = new KeyEventDispatcher() {
 
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent evt) {
+				if(evt.getID() == KeyEvent.KEY_PRESSED) {
+					
+					String direction = null;
+					switch(evt.getKeyCode()){
+					case KeyEvent.VK_K:
+						direction = "bottom";
+						break;
+					case KeyEvent.VK_J:
+						direction = "left";
+						break;
+					case KeyEvent.VK_L:
+						direction = "right";
+						break;
+					case KeyEvent.VK_I:
+						direction = "top";
+						break;
+					}
+					
+					if(direction != null){
+						GestureMessage gm = new GestureMessage(GestureType.SWIPE, direction);
+						if(currentApp != null)
+							currentApp.getClient().sendCommand(gm);
+					}
+					
+					
+				}
+				return false;
+			}
+
+		};
+KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(listener);
+	}
 }
+
