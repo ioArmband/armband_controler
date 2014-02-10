4ee1083d5a6fad0ab3cc5319d66b6f5779c94262
VirtualInstict <jean-michel@pothelune.fr>
2014-02-10 03:36:16 +0100

    Add buttons to SlideSwiperApp

--- src/main/java/org/tse/pri/ioarmband/armband/apps/impl/SlideSwiperApp.java
+++ src/main/java/org/tse/pri/ioarmband/armband/apps/impl/SlideSwiperApp.java
@@ -4,14 +4,20 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.tse.pri.ioarmband.armband.apps.GenericSwingApp;
 import org.tse.pri.ioarmband.armband.io.Client;
+import org.tse.pri.ioarmband.io.message.Command;
+import org.tse.pri.ioarmband.io.message.GestureMessage;
+import org.tse.pri.ioarmband.io.message.enums.GestureType;
 
-public class SlideSwiperApp extends GenericSwingApp{
+public class SlideSwiperApp extends GenericSwingApp implements MouseListener{
 
 	public SlideSwiperApp(Client client) {
 		super(client);
@@ -20,9 +26,23 @@ public class SlideSwiperApp extends GenericSwingApp{
 
 	public void build(Container container){
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(1,1));
+		panel.setLayout(new GridLayout(1,2));
 		panel.setBackground(Color.BLACK);
-		
+
+		BlackButton button;
+		button = new BlackButton("<<");
+		button.setName("left");
+		button.addMouseListener(this);
+
+		Font font = new Font(button.getFont().getName(), button.getFont().getStyle(), 200);
+		button.setFont(font);
+		panel.add(button);
+		button = new BlackButton(">>");
+		button.setName("right");
+		button.addMouseListener(this);
+		button.setFont(font);
+		panel.add(button);
+		/*
 		JLabel label = new JLabel("<<        >>", JLabel.CENTER);
 		label.setVerticalAlignment(JLabel.CENTER);
 		Font font = new Font(label.getFont().getName(), label.getFont().getStyle(), 200);
@@ -30,10 +50,33 @@ public class SlideSwiperApp extends GenericSwingApp{
 		label.setFont(font);
 		label.setSize(panel.getSize());
 		panel.add(label);
+		*/
 		
 		container.add(panel);
 	}
 	
 	public void hide() {
 	}
+	
+	@Override
+	public void mousePressed(MouseEvent e){
+		GestureType gestureType = GestureType.TOUCH;
+		String source = e.getComponent().getName();
+		
+		dispatchGesture(gestureType, source);
+		
+		GestureMessage message = new GestureMessage();
+		message.setSourceName(source);
+		message.setType(gestureType);
+		client.sendCommand(message);
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
 }
